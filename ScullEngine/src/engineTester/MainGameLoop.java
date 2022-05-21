fbd36b1aa5aa464e85307a5a753ed6049b751b35
alterkait <alterkait2001@icloud.com>
2022-05-21 22:22:06 +0100

    Prepare Loader & Renderer into Main loop

--- ScullEngine/src/engineTester/MainGameLoop.java
+++ ScullEngine/src/engineTester/MainGameLoop.java
@@ -3,16 +3,25 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 	
 	public static void main(String [] args) {
 		DisplayManager.createDisplay();
 		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		
 		while(!Display.isCloseRequested()) {
+			renderer.prepare();
+			
 			DisplayManager.updateDisplay();
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 	
