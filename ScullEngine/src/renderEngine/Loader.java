e0c6dca28bff4282baa09f08766f4fdff65cb1a6
alterkait <alterkait2001@icloud.com>
2022-05-21 21:52:14 +0100

    init Class to load 3D model into memory

---
+++ ScullEngine/src/renderEngine/Loader.java
@@ -0,0 +1,25 @@
+package renderEngine;
+
+/*
+ * Loading 3D models into memory by storing positional data 
+ * about the model in a VAO
+ */
+public class Loader {
+	
+	public RawModel loadToVAO(float[] positions) {
+		
+	}
+	
+	private int createVAO() {
+		
+	}
+	
+	private void storeDataInAttributeList(int attributeNumber, float[] data) {
+		
+	}
+	
+	private void unbindVAO() {
+		
+	}
+
+}
