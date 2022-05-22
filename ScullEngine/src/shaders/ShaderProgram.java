e42c21d2c8a98fb07566ed1afe0013d9e7820926
alterkait <alterkait2001@icloud.com>
2022-05-22 01:42:57 +0100

    define vertex and fragment shaders

--- ScullEngine/src/shaders/ShaderProgram.java
+++ ScullEngine/src/shaders/ShaderProgram.java
@@ -18,6 +18,41 @@ public abstract class ShaderProgram {
 	private int vertexShaderID;
 	private int fragmentShaderID;
 	
+	public ShaderProgram(String vertexFile, String fragmentFile) {
+		vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
+		fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
+		
+		programID = GL20.glCreateProgram();
+		
+		GL20.glAttachShader(programID, vertexShaderID);
+		GL20.glAttachShader(programID, fragmentShaderID);
+		GL20.glLinkProgram(programID);
+		GL20.glValidateProgram(programID);
+	}
+	
+	public void start() {
+		GL20.glUseProgram(programID);
+	}
+	
+	public void stop() {
+		GL20.glUseProgram(0);
+	}
+	
+	public void cleanUp() {
+		stop();
+		GL20.glDetachShader(programID, vertexShaderID);
+		GL20.glDetachShader(programID, fragmentShaderID);
+		GL20.glDeleteShader(vertexShaderID);
+		GL20.glDeleteShader(fragmentShaderID);
+		GL20.glDeleteProgram(programID);
+	}
+	
+	protected abstract void bindAttributes();
+	
+	protected void bindAttribute(int attribute, String variableName) {
+		GL20.glBindAttribLocation(programID, attribute, variableName);
+	}
+	
 	private static int loadShader(String file, int type) {
 		StringBuilder shaderSource = new StringBuilder();
 		
