49dde782655d96a8135af394d57d96c784bb8ccd
alterkait <alterkait2001@icloud.com>
2022-05-22 01:46:44 +0100

    Static shader objects in main loop

--- ScullEngine/src/engineTester/MainGameLoop.java
+++ ScullEngine/src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 	
@@ -14,6 +15,7 @@ public class MainGameLoop {
 		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
 		float[] vertices = {
 				-0.5f,-0.5f,0f,		//vertex 1
@@ -33,11 +35,14 @@ public class MainGameLoop {
 		
 		while(!Display.isCloseRequested()) {
 			renderer.prepare();
+			shader.start();
 			
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
