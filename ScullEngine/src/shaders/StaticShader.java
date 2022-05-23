5021ddf08b9d3ce8265292f9157e4caee75694f2
alterkait <alterkait2001@icloud.com>
2022-05-23 06:09:55 +0100

    init transformation matrix

--- ScullEngine/src/shaders/StaticShader.java
+++ ScullEngine/src/shaders/StaticShader.java
@@ -1,10 +1,15 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
 	
 	private static final String VERTEX_FILE = "C:\\Users\\alter\\git\\repository2\\ScullEngine\\src\\shaders\\vertexShader.txt";
 	private static final String FRAGMENT_FILE = "C:\\Users\\alter\\git\\repository2\\ScullEngine\\src\\shaders\\fragmentShader.txt";
 
+	
+	private int location_transformationMatrix;
+	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 	}
@@ -12,5 +17,15 @@ public class StaticShader extends ShaderProgram{
 	protected void bindAttributes() {
 		super.bindAttribute(0, "position");
 		super.bindAttribute(1, "textureCoords");
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+		
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_transformationMatrix, matrix);
 	}
 }
