50788939b70793968178943e2f757b95f79b64a7
alterkait <alterkait2001@icloud.com>
2022-05-24 02:54:44 +0100

    fix vertices + indices ordering

--- ScullEngine/src/engineTester/MainGameLoop.java
+++ ScullEngine/src/engineTester/MainGameLoop.java
@@ -1,7 +1,9 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -16,20 +18,16 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
+		
 		StaticShader shader = new StaticShader();
 		
-		float[] vertices = {
-				-0.5f,-0.5f,0f,		//vertex 1
-				0.5f, -0.5f, 0f,	//vertex 2
-				0.5f, 0.5f, 0f,		//vertex 3
-				-0.5f, 0.5f, 0f		//vertex 4
-				
-		};
+		Renderer renderer = new Renderer(shader);
 		
-		int[] indices = {
-				0,1,2,
-				0,2,3
+		float[] vertices = {
+				-0.5f,0.5f,0f,		//vertex 1
+				-0.5f, -0.5f, 0f,	//vertex 2
+				0.5f, -0.5f, 0f,		//vertex 3
+				0.5f, 0.5f, 0f		//vertex 4
 				
 		};
 		
@@ -40,15 +38,27 @@ public class MainGameLoop {
 				1,0
 		};
 		
+		int[] indices = {
+				0,1,3,
+				3,1,2
+				
+		};
+		
+		
+		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("bee"));
-		TexturedModel texturedModel = new TexturedModel(model,texture);
+		
+		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("bird")));
+		
+		Entity entity = new Entity(staticModel, new Vector3f(0,0,-1),0,0,0,1);
 		
 		while(!Display.isCloseRequested()) {
+			entity.increasePosition(0, 0, -0.1f);
+			
 			renderer.prepare();
 			shader.start();
 			
-			renderer.render(texturedModel);
+			renderer.render(entity,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
