4cf3cd09164790aa32613f70f1671bbd2c88484b
alterkait <alterkait2001@icloud.com>
2022-05-24 03:58:04 +0100

    view matrix math

--- ScullEngine/src/toolbox/Mathematics.java
+++ ScullEngine/src/toolbox/Mathematics.java
@@ -3,6 +3,8 @@ package toolbox;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+
 public class Mathematics {
 	
 	public static Matrix4f createTransformationMatrix(Vector3f translation, 
@@ -19,6 +21,24 @@ public class Mathematics {
 		
 		return matrix;
 	
+	}
+	
+	public Matrix4f createViewMatrix(Camera camera) {
+		Matrix4f viewMatrix = new Matrix4f();
+		viewMatrix.setIdentity();
+		
+		Matrix4f.rotate((float) Math.toRadians(camera.getPitch()),
+				new Vector3f(1,0,0), viewMatrix, viewMatrix);
+		
+		Matrix4f.rotate((float) Math.toRadians(camera.getYaw()),
+				new Vector3f(0,1,0), viewMatrix, viewMatrix);
+		
+		Vector3f cameraPos = camera.getPosition();
+		Vector3f negativeCameraPos = new Vector3f(-cameraPos.x, -cameraPos.y, -cameraPos.z);
+		Matrix4f.translate(negativeCameraPos, viewMatrix, viewMatrix);
+		
+		return viewMatrix;
+		
 	}
 
 }
