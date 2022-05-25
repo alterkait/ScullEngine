454cdf1f29f70c8aa47a775298436c60c53b44d3
alterkait <alterkait2001@icloud.com>
2022-05-25 13:38:40 +0100

    Get light position and color

--- ScullEngine/src/shaders/StaticShader.java
+++ ScullEngine/src/shaders/StaticShader.java
@@ -3,6 +3,7 @@ package shaders;
 import org.lwjgl.util.vector.Matrix4f;
 
 import entities.Camera;
+import entities.Light;
 import toolbox.Mathematics;
 
 public class StaticShader extends ShaderProgram{
@@ -14,6 +15,8 @@ public class StaticShader extends ShaderProgram{
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;
 	private int location_viewMatrix;
+	private int location_lightPosition;
+	private int location_lightColor;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -29,6 +32,9 @@ public class StaticShader extends ShaderProgram{
 		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
 		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
 		location_viewMatrix = super.getUniformLocation("viewMatrix");
+		location_lightPosition = super.getUniformLocation("lightPosition");
+		location_lightColor = super.getUniformLocation("lightColor");
+		
 	}
 	
 	public void loadTransformationMatrix(Matrix4f matrix) {
@@ -42,5 +48,10 @@ public class StaticShader extends ShaderProgram{
 	public void loadViewMatrix(Camera camera) {
 		Matrix4f viewMatrix = Mathematics.createViewMatrix(camera);
 		super.loadMatrix(location_viewMatrix, viewMatrix);
+	}
+	
+	public void loadLight(Light light) {
+		super.loadVector(location_lightPosition, light.getPosition());
+		super.loadVector(location_lightColor, light.getColor());
 	}
 }
