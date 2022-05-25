4d24710ba5b7b9af5eeacab1c7eac6e7258114ec
alterkait <alterkait2001@icloud.com>
2022-05-25 14:32:40 +0100

    Specular Lighting

--- ScullEngine/src/shaders/StaticShader.java
+++ ScullEngine/src/shaders/StaticShader.java
@@ -17,6 +17,8 @@ public class StaticShader extends ShaderProgram{
 	private int location_viewMatrix;
 	private int location_lightPosition;
 	private int location_lightColor;
+	private int location_shineDamper;
+	private int location_reflectivity;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -35,6 +37,8 @@ public class StaticShader extends ShaderProgram{
 		location_viewMatrix = super.getUniformLocation("viewMatrix");
 		location_lightPosition = super.getUniformLocation("lightPosition");
 		location_lightColor = super.getUniformLocation("lightColor");
+		location_shineDamper = super.getUniformLocation("shineDamper");
+		location_reflectivity = super.getUniformLocation("reflectivity");
 		
 	}
 	
@@ -54,5 +58,11 @@ public class StaticShader extends ShaderProgram{
 	public void loadLight(Light light) {
 		super.loadVector(location_lightPosition, light.getPosition());
 		super.loadVector(location_lightColor, light.getColor());
+	}
+	
+	public void loadShineVariables(float damper, float reflectivity) {
+		super.loadFloat(location_shineDamper, damper);
+		super.loadFloat(location_reflectivity, reflectivity);
+		
 	}
 }
