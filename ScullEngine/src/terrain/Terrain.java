bf5b3e25b0345d7c895da0b2eebe6e6b4319328d
alterkait <alterkait2001@icloud.com>
2022-11-06 18:27:01 +0000

    Terrain resizing

--- ScullEngine/src/terrain/Terrain.java
+++ ScullEngine/src/terrain/Terrain.java
@@ -6,8 +6,8 @@ import textures.ModelTexture;
 
 public class Terrain {
 	
-	private static final float SIZE = 1600;
-	private static final int VERTEX_COUNT = 256;
+	private static final float SIZE = 800;
+	private static final int VERTEX_COUNT = 128;
 	
 	private float x;
 	private float z;
